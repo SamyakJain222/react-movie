c90b9d620e9988bcb43d9db87f34406c71703a0d
SamyakJain222 <Samyak24702Jain@gmail.com>
2023-10-26 21:56:16 +0530

    initial deployment

---
+++ precache-manifest.bd77496fcc8ee1bdb0eca97e4c4c8338.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a0a0d17e04a3377827c3c19c88b1fb",
+    "url": "/react-movie/index.html"
+  },
+  {
+    "revision": "f1bea317dc31213c3c57",
+    "url": "/react-movie/static/css/main.c923e330.chunk.css"
+  },
+  {
+    "revision": "9b6e0aa8b2de77432b9a",
+    "url": "/react-movie/static/js/2.2c4dd873.chunk.js"
+  },
+  {
+    "revision": "f1bea317dc31213c3c57",
+    "url": "/react-movie/static/js/main.82d8bbf5.chunk.js"
+  },
+  {
+    "revision": "6d4ba1826c20ec65bbc7",
+    "url": "/react-movie/static/js/runtime-main.392ab6e0.js"
+  }
+]);
